4fb5684979352080e18691b63f6c984be8d6417c
AnnikaChauhan <annikac0711@gmail.com>
2021-12-29 18:42:52 +0000

    updates package jsons and personalises packages

--- packages/client-two/src/App.js
+++ packages/client-two/src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import { Button } from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
       <h1>Client Two</h1>
+      <Button variant="outlined" color="primary">
+        Click Two
+      </Button>
     </div>
   );
 }
--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import { Button } from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
       <h1>Client ONE</h1>
+      <Button variant="outlined" color="primary">
+        Click One
+      </Button>
     </div>
   );
 }
